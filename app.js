69f6fa000cbcc4749a3bf45a24408e064f3335ba
Fabian <garciafabianemmanuel@gmail.com>
2023-08-26 22:05:42 -0300

    configuracion y conexion con la base de datos

--- app.js
+++ app.js
@@ -7,18 +7,22 @@ const morgan = require('morgan');
 const path = require('path');
 require('dotenv').config();
 
+// Se conecta la Base de Datos
+const { conectarDB } = require('./database');
+conectarDB()
+
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 
 // Middlewares
-// TODO: Implementar middlewares
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 
+// Routes
 
 // Starting the server
 app.listen(port, () => console.log(`Servidor en el puerto ${port}`));
